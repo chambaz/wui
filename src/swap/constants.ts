fa2225447c605b090d8bf15f4a80aa8be2f41ae9
Adam Chambers <adammchambers@gmail.com>
2026-04-18 20:29:26 -0400

    fix: jup referral fees

--- src/swap/constants.ts
+++ src/swap/constants.ts
@@ -1,9 +1,8 @@
 export const PLATFORM_FEE_BPS = 30;
 /** Default max slippage percentage shown in the swap UI. */
 export const DEFAULT_SLIPPAGE_PCT = "0.50";
-export const FEE_WALLET_ADDRESS = "3TZ4bdHjJaxempMDU9n7itvCEsFVYg5TyzjNWnvT5i6X";
-export const TOKEN_PROGRAM = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
-export const ATA_PROGRAM = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
+export const REFERRAL_ACCOUNT_ADDRESS = "3TZ4bdHjJaxempMDU9n7itvCEsFVYg5TyzjNWnvT5i6X";
+export const JUPITER_REFERRAL_PROGRAM = "REFER4ZgmyYx9c6He5XfaTMiGfdLwRnkV4RPp9t9iF3";
 export const MAX_PRIORITY_FEE_LAMPORTS = 1_000_000;
 export const CONFIRMATION_POLL_INTERVAL_MS = 2_000;
 export const CONFIRMATION_TIMEOUT_MS = 60_000;
